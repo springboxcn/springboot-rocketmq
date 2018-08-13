91565aa20e2f4181f5d370a4c5f625023f3a2f21
maskleo <masque@aliyun.com>
2018-08-13 14:26:15 +0800

    add Broadcast Producer

---
+++ client/src/main/java/edu/maskleo/client/broadcasting/BroadcastProducer.java
@@ -0,0 +1,29 @@
+package edu.maskleo.client.broadcasting;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * 广播正在向主题的所有订阅者发送消息。 如果您希望所有订阅者都收到有关主题的消息，则广播是一个不错的选择。
+ * <p>
+ * 假如多个消费者同时订阅一个生产者，默认是只有一个消费者拿到消息，广播是每个订阅了的消费者都拿到消息
+ */
+public class BroadcastProducer {
+
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("ProducerGroupName");
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            Message msg = new Message("TopicTest",
+                    "TagA",
+                    "OrderID188",
+                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+
+}
