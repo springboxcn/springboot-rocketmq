f15ecf04ec3846c7cc2e8f14dc97035067c79d2e
maskleo <masque@aliyun.com>
2018-08-14 15:53:40 +0800

    add Schedule Example

---
+++ client/src/main/java/edu/maskleo/client/schedule/ScheduledMessageConsumer.java
@@ -0,0 +1,30 @@
+package edu.maskleo.client.schedule;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+
+/**
+ * Scheduled messages differ from normal messages in that they won’t be delivered until a provided time later.
+ */
+public class ScheduledMessageConsumer {
+
+    public static void main(String[] args) throws Exception {
+        // Instantiate message consumer
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ExampleConsumer");
+        // Subscribe topics
+        consumer.subscribe("TestTopic", "*");
+        // Register message listener
+        consumer.registerMessageListener((MessageListenerConcurrently) (messages, context) -> {
+            for (MessageExt message : messages) {
+                // Print approximate delay time period
+                System.out.println("Receive message[msgId=" + message.getMsgId() + "] "
+                        + (System.currentTimeMillis() - message.getStoreTimestamp()) + "ms later");
+            }
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        // Launch consumer
+        consumer.start();
+    }
+}
